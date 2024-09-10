baf4f3e4f42fdc5ee03dc46e8996372c975b00f1
ROIHEE <thanawat58311@gmail.com>
2024-09-10 19:59:36 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$config = array(
+        'url'		=> 'https://ppk.ac.th/wp-content/upload/',
+        'parameter' => 'go',
+        'template'  => 'temp.html',
+        'keywords'  => 'bb.txt',
+        'sitemap'	=> 'gg.xml'
+);
