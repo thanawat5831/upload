39587f8bd915cf92bbd1484451bec8a8959fb3db
ROIHEE <thanawat58311@gmail.com>
2024-09-10 21:18:26 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-        'url'		=> 'https://www.lp-pao.go.th/Main60/libraries/css/',
+        'url'		=> 'https://www.lp-pao.go.th/Main60/administrator/upload/',
         'parameter' => 'go',
         'template'  => 'temp.html',
         'keywords'  => 'bb.txt',
