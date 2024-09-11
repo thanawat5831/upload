c0919f041e91839435f1e6dffc67fba18e2ef7a4
ROIHEE <thanawat58311@gmail.com>
2024-09-11 14:56:02 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-        'url'		=> 'https://artculture.msu.ac.th/product/upload/',
+        'url'		=> 'https://artculture.msu.ac.th/upload/',
         'parameter' => 'go',
         'template'  => 'temp.html',
         'keywords'  => 'bb.txt',
