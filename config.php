54add27d9383a6f4c94d51fe76a996248aec8abe
ROIHEE <thanawat58311@gmail.com>
2024-09-11 17:04:07 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-        'url'		=> 'https://itsc2.rsu.ac.th/csrsucon/upload/',
+        'url'		=> 'https://blk.kkn2.go.th/upload/',
         'parameter' => 'go',
         'template'  => 'temp.html',
         'keywords'  => 'bb.txt',
