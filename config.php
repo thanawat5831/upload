31b5da6cd1accf0d78d7688786cfc2121b130a14
ROIHEE <thanawat58311@gmail.com>
2024-09-12 17:53:02 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-        'url'		=> 'https://bdy.kkn2.go.th/upload/',
+        'url'		=> 'https://bks.kkn2.go.th/upload/',
         'parameter' => 'go',
         'template'  => 'temp.html',
         'keywords'  => 'bb.txt',
