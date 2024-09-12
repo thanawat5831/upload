de7434adf7984a95847f8569cb64747f1dc191b0
ROIHEE <thanawat58311@gmail.com>
2024-09-12 18:47:31 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-        'url'		=> 'https://bmp.kkn2.go.th/upload/',
+        'url'		=> 'https://bnk.kkn2.go.th/upload/',
         'parameter' => 'go',
         'template'  => 'temp.html',
         'keywords'  => 'bb.txt',
