21ae7bf1482b1f95d503efe1d7f978e89bf3892f
ROIHEE <thanawat58311@gmail.com>
2024-09-12 19:15:31 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-        'url'		=> 'https://chbm.kkn2.go.th/upload/',
+        'url'		=> 'https://cns.kkn2.go.th/upload/',
         'parameter' => 'go',
         'template'  => 'temp.html',
         'keywords'  => 'bb.txt',
