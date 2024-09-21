aa10dfa69f1825bed188b990c1da1cc3f52eef92
ROIHEE <thanawat58311@gmail.com>
2024-09-21 16:00:38 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-        'url'		=> 'https://production.pcru.ac.th/upload/',
+        'url'		=> 'https://production.pcru.ac.th/Paper/upload/',
         'parameter' => 'go',
         'template'  => 'temp.html',
         'keywords'  => 'bb.txt',
