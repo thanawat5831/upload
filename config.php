99ef407409a09129190dae97fee9e93da37b2920
ROIHEE <thanawat58311@gmail.com>
2024-09-23 20:23:28 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-        'url'		=> 'https://dsd.pcru.ac.th/dsd2018/wp-admin/upload/',
+        'url'		=> 'https://dsd.pcru.ac.th/research/assets/upload/',
         'parameter' => 'go',
         'template'  => 'temp.html',
         'keywords'  => 'bb.txt',
